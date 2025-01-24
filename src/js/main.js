c917e626364bd13901367fd54b467f8b47147b17
julia <julia.zhyvylo@asila.mk>
2025-01-24 22:16:17 +0100

    add styles and limit size

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 // import "./category/example.js";    example of your import
 import './header-menu.js';
 import './quote.js';
-import './search.js';
-import './favorites.js';
-
-
+// import './search.js';
 import "./category/exercises-list.js";
+// import './favorites.js';
+
