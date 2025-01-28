a8eaf05181f940e202d5ddfe5646dde61f9a1883
MaRo-Christmas <152757506+MaRo-Christmas@users.noreply.github.com>
2025-01-28 13:49:22 +0000

    Deploying to gh-pages from @ MaRo-Christmas/desperate-pancakes-development@6204f29cd403244ee6a0d21e8c8aca13f02ef7a5 🚀

--- assets/modal-window-BSX-Iyga.js
+++ assets/modal-window-BSX-Iyga.js
@@ -10,4 +10,4 @@ import{a as N}from"./vendor-CQpva9gi.js";(function(){const t=document.createElem
     <p class='modal-info-targets-list-item-container-subtitle'>${i[r].subtitle}</p>
     </div>
   </li>`).join(""),W.innerHTML=`<p>${n.description}</p>`,c.classList.add("is-open")}}export{Y as h,X as i};
-//# sourceMappingURL=modal-window-DFfdQdUV.js.map
+//# sourceMappingURL=modal-window-BSX-Iyga.js.map
